9a5163c3164ab17cf6bf7203f6ee536125e70bd2
zhihaofans <zhihaofans@hotmail.com>
2021-02-20 10:59:24 +0800

    Update $_.js

--- scripts/libs/$_.js
+++ scripts/libs/$_.js
@@ -39,6 +39,25 @@ class ModV4 {
     }
 }
 const url = {
+    regex: _inputUrl => {
+        // Example: https://github.com/cyanzhong/xTeko/tree/master/extension-demos/keyboard
+        const regex = /(http|https):\/\/github.com\/(.+)\/(tree|blob)\/(.+?)\/(.+)/;
+        const matches = regex.exec(_inputUrl);
+        if (matches) {
+            const repo = matches[2];
+            const branch = matches[4];
+            const path = matches[5];
+            const result = `https://api.github.com/repos/${repo}/contents/${path}?ref=${branch}`;
+        } else {
+            // Example: https://github.com/cyanzhong/xTeko
+            if (/(http|https):\/\/github.com\/(.+)\/([^/]+)/.test(_inputUrl)) {
+                // Try master branch,
+                // if the default branch isn't master, it will fail
+                const repo = url.split("/").slice(0, 5).join("/");
+                const result = `${repo}/archive/master.zip`;
+            }
+        }
+    },
     split: _inputUrl => {
         if (url.isUrl(_inputUrl)) {
             //        const schemeSymbol;
