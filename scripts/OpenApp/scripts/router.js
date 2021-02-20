ff27ba97680d3bbfd0de45d647d3238313551452
zhihaofans <zhihaofans@hotmail.com>
2021-02-20 10:27:11 +0800

    OpenApp: Update router.js

--- scripts/OpenApp/scripts/router.js
+++ scripts/OpenApp/scripts/router.js
@@ -7,49 +7,70 @@ const JSDialogs = require("./scripts/libs/dialogs").Dialogs,
         }
     },
     url = {
+        "www.v2ex.com": {
+            "/t/": "v2er.topic"
+        },
         "v2ex.com": {
             "/t/": "v2er.topic"
         }
     },
-    checkRouter = _url => {
-        const _u = new URL(_url);
-        const host = _u.hostname,
-            pathname = _u.pathname;
-        $console.error(_u);
-        if (url[host]) {
-            const pathList = Object.keys(url[host]);
-            let itemKey = undefined,
-                i = 0;
-            do {
-                if (pathname.startsWith(pathList[i])) {
-                    itemKey = url[host][pathList[i]];
-                }
-                i += 1;
-            } while (itemKey == undefined);
-            return itemKey;
-        } else {
-            return undefined;
-        }
-    },
-    goRouter = (routerId, _url) => {
-        if (routerId && _url) {
-            const routerData = app[routerId];
-            if (routerData) {
-                const jsPath = `/scripts/${routerData.type}.js`;
-                if ($file.exists(jsPath)) {
-                    const routerJs = require(jsPath);
-                    routerJs[routerData.app][routerData.func](_url);
+    checkRouter = async _url => {
+            try {
+                const _u = new URL(_url);
+                const host = _u.hostname,
+                    pathname = _u.pathname;
+                $console.warn(_url);
+                $console.error(_u.toString());
+                if (url[host]) {
+                    let pathList = Object.keys(url[host]),
+                        itemKey = undefined,
+                        i = 0;
+                    do {
+                        const thisItem = pathList[i];
+                        if (pathname.startsWith(thisItem)) {
+                            itemKey = url[host][thisItem];
+                        }
+                        i += 1;
+                    } while (itemKey == undefined);
+                    return itemKey;
                 } else {
-                    JSDialogs.showPlainAlert("不存在该文件", jsPath);
+                    return undefined;
                 }
-            } else {
-                JSDialogs.showPlainAlert("错误", "未知路由");
+            } catch (_error) {
+                $console.error(_error.message);
+                await JSDialogs.showPlainAlert("try catch", _error.message);
+                return undefined;
             }
-        }
-    };
+        },
+        goRouter = (routerId, _url) => {
+            if (routerId && _url) {
+                const routerData = app[routerId];
+                if (routerData) {
+                    const jsPath = `/scripts/${routerData.type}.js`;
+                    if ($file.exists(jsPath)) {
+                        const routerJs = require(jsPath);
+                        routerJs[routerData.app][routerData.func](_url);
+                    } else {
+                        JSDialogs.showPlainAlert("不存在该文件", jsPath);
+                    }
+                } else {
+                    JSDialogs.showPlainAlert("错误", "未知路由");
+                }
+            }
+        },
+        init = _url => {
+            const routerKey = checkRouter(_url);
+            if (routerKey) {
+                $console.info(`routerKey:${routerKey}`);
+                goRouter(routerKey, _url);
+            } else {
+                $ui.error("找不到支持该链接的路由");
+            }
+        };
 module.exports = {
     app,
     url,
     checkRouter,
-    goRouter
+    goRouter,
+    init
 };
