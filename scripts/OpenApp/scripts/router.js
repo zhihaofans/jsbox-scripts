05d1cbbf2b557be8b06b90cb8dbbd6c25a78158a
zhihaofans <zhihaofans@hotmail.com>
2021-02-20 10:59:15 +0800

    Update

--- scripts/OpenApp/scripts/router.js
+++ scripts/OpenApp/scripts/router.js
@@ -15,58 +15,58 @@ const JSDialogs = require("./scripts/libs/dialogs").Dialogs,
         }
     },
     checkRouter = async _url => {
-            try {
-                const _u = new URL(_url);
-                const host = _u.hostname,
-                    pathname = _u.pathname;
-                $console.warn(_url);
-                $console.error(_u.toString());
-                if (url[host]) {
-                    let pathList = Object.keys(url[host]),
-                        itemKey = undefined,
-                        i = 0;
-                    do {
-                        const thisItem = pathList[i];
-                        if (pathname.startsWith(thisItem)) {
-                            itemKey = url[host][thisItem];
-                        }
-                        i += 1;
-                    } while (itemKey == undefined);
-                    return itemKey;
-                } else {
-                    return undefined;
-                }
-            } catch (_error) {
-                $console.error(_error.message);
-                await JSDialogs.showPlainAlert("try catch", _error.message);
+        try {
+            const _u = new URL(_url);
+            const host = _u.hostname,
+                pathname = _u.pathname;
+            $console.warn(_url);
+            $console.error(_u.toString());
+            if (url[host]) {
+                let pathList = Object.keys(url[host]),
+                    itemKey = undefined,
+                    i = 0;
+                do {
+                    const thisItem = pathList[i];
+                    if (pathname.startsWith(thisItem)) {
+                        itemKey = url[host][thisItem];
+                    }
+                    i += 1;
+                } while (itemKey == undefined);
+                return itemKey;
+            } else {
                 return undefined;
             }
-        },
-        goRouter = (routerId, _url) => {
-            if (routerId && _url) {
-                const routerData = app[routerId];
-                if (routerData) {
-                    const jsPath = `/scripts/${routerData.type}.js`;
-                    if ($file.exists(jsPath)) {
-                        const routerJs = require(jsPath);
-                        routerJs[routerData.app][routerData.func](_url);
-                    } else {
-                        JSDialogs.showPlainAlert("不存在该文件", jsPath);
-                    }
+        } catch (_error) {
+            $console.error(_error.message);
+            await JSDialogs.showPlainAlert("try catch", _error.message);
+            return undefined;
+        }
+    },
+    goRouter = (routerId, _url) => {
+        if (routerId && _url) {
+            const routerData = app[routerId];
+            if (routerData) {
+                const jsPath = `/scripts/${routerData.type}.js`;
+                if ($file.exists(jsPath)) {
+                    const routerJs = require(jsPath);
+                    routerJs[routerData.app][routerData.func](_url);
                 } else {
-                    JSDialogs.showPlainAlert("错误", "未知路由");
+                    JSDialogs.showPlainAlert("不存在该文件", jsPath);
                 }
-            }
-        },
-        init = _url => {
-            const routerKey = checkRouter(_url);
-            if (routerKey) {
-                $console.info(`routerKey:${routerKey}`);
-                goRouter(routerKey, _url);
             } else {
-                $ui.error("找不到支持该链接的路由");
+                JSDialogs.showPlainAlert("错误", "未知路由");
             }
-        };
+        }
+    },
+    init = _url => {
+        const routerKey = checkRouter(_url);
+        if (routerKey) {
+            $console.info(`routerKey:${routerKey}`);
+            goRouter(routerKey, _url);
+        } else {
+            $ui.error("找不到支持该链接的路由");
+        }
+    };
 module.exports = {
     app,
     url,
