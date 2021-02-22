523ae1f5cb9d05c8d07ac7fbbe86e5749f1fda9a
zhihaofans <zhihaofans@hotmail.com>
2021-02-23 01:39:39 +0800

    $_.js: Update

--- scripts/libs/$_.js
+++ scripts/libs/$_.js
@@ -3,7 +3,7 @@ class ModV4 {
         this._indexJsPath = indexJsPath;
     }
     get indexJs() {
-        const _f = require("$$").File;
+        const _f = $$.File;
         return this._indexJsPath
             ? _f.isFile(this.vindexJsPath)
                 ? require(this._indexJsPath) || undefined
@@ -38,65 +38,73 @@ class ModV4 {
         });
     }
 }
-const Url = {
-    regex: inputUrl => {
-        let _inputUrl = inputUrl,
-            fragmentIndex = _inputUrl.indexOf("#"),
-            fragment = undefined,
-            queryIndex = _inputUrl.indexOf("?"),
-            queryStr = undefined,
-            queryObj = {};
-        if (fragmentIndex >= 0) {
-            fragment = _inputUrl.substring(fragmentIndex + 1, _inputUrl.length);
-            _inputUrl = _inputUrl.substring(0, fragmentIndex);
-        }
-        if (queryIndex >= 0) {
-            queryStr = _inputUrl.substring(queryIndex + 1, _inputUrl.length);
-            _inputUrl = _inputUrl.substring(0, queryIndex);
-            queryStr.split("&").map(i => {
-                if (i.indexOf("=")) {
-                    const _queryItem = i.split("=");
-                    if (_queryItem.length == 2) {
-                        queryObj[_queryItem[0]] = _queryItem[1];
+const $$ = require("$$"),
+    Url = {
+        regex: inputUrl => {
+            let _inputUrl = inputUrl,
+                fragmentIndex = _inputUrl.indexOf("#"),
+                fragment = undefined,
+                queryIndex = _inputUrl.indexOf("?"),
+                queryStr = undefined,
+                queryObj = {};
+            if (fragmentIndex >= 0) {
+                fragment = _inputUrl.substring(
+                    fragmentIndex + 1,
+                    _inputUrl.length
+                );
+                _inputUrl = _inputUrl.substring(0, fragmentIndex);
+            }
+            if (queryIndex >= 0) {
+                queryStr = _inputUrl.substring(
+                    queryIndex + 1,
+                    _inputUrl.length
+                );
+                _inputUrl = _inputUrl.substring(0, queryIndex);
+                queryStr.split("&").map(i => {
+                    if (i.indexOf("=")) {
+                        const _queryItem = i.split("=");
+                        if (_queryItem.length == 2) {
+                            queryObj[_queryItem[0]] = _queryItem[1];
+                        }
                     }
-                }
-            });
-        } else {
-            queryObj = undefined;
+                });
+            } else {
+                queryObj = undefined;
+            }
+            const regex = /(.+):\/\/(.+?)\/(.+)/,
+                matches = regex.exec(_inputUrl);
+            if (matches) {
+                const scheme = matches[1],
+                    host = matches[2],
+                    path = matches[3],
+                    urlResult = {
+                        scheme: scheme,
+                        host: host,
+                        path: path.startsWith("/") ? path : `/${path}`,
+                        fragment: fragment,
+                        query: queryObj,
+                        queryStr: queryStr
+                    };
+                return urlResult;
+            } else {
+                return matches || undefined;
+            }
+        },
+        split: _inputUrl => {
+            if (Url.isUrl(_inputUrl)) {
+                //        const schemeSymbol;
+                return {};
+            } else {
+                return undefined;
+            }
+        },
+        isUrl: _inputUrl => {
+            const _urlResult = Url.regex(_inputUrl);
+            return _urlResult ? _urlResult.host && _urlResult.scheme : false;
         }
-        const regex = /(.+):\/\/(.+?)\/(.+)/,
-            matches = regex.exec(_inputUrl);
-        if (matches) {
-            const scheme = matches[1],
-                host = matches[2],
-                path = matches[3],
-                urlResult = {
-                    scheme: scheme,
-                    host: host,
-                    path: path.startsWith("/") ? path : `/${path}`,
-                    fragment: fragment,
-                    query: queryObj,
-                    queryStr: queryStr
-                };
-            return urlResult;
-        } else {
-            return matches || undefined;
-        }
-    },
-    split: _inputUrl => {
-        if (Url.isUrl(_inputUrl)) {
-            //        const schemeSymbol;
-            return {};
-        } else {
-            return undefined;
-        }
-    },
-    isUrl: _inputUrl => {
-        const _urlResult = Url.regex(_inputUrl);
-        return _urlResult ? _urlResult.host && _urlResult.scheme : false;
-    }
-};
+    };
 module.exports = {
     ModV4,
-    Url
+    Url,
+    $$
 };
