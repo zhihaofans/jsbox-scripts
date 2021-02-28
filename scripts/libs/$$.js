65fc68a9340527cc5d667afdac812e943ec4da39
zhihaofans <zhihaofans@hotmail.com>
2021-02-28 10:35:59 +0800

    $$.js: Add Url

--- scripts/libs/$$.js
+++ scripts/libs/$$.js
@@ -1,4 +1,4 @@
-let __VERSION__ = 1,
+let __VERSION__ = 2,
     Http = {
         getAwait: async (url, header) => {
             const result = await $http.get({
@@ -301,6 +301,62 @@ let __VERSION__ = 1,
                 ]
             });
         }
+    },
+    Url = {
+        getUrlByRegex: inputUrl => {
+            let _inputUrl = inputUrl,
+                fragmentIndex = _inputUrl.indexOf("#"),
+                fragment = undefined,
+                queryIndex = _inputUrl.indexOf("?"),
+                queryStr = undefined,
+                queryObj = {};
+            if (fragmentIndex >= 0) {
+                fragment = _inputUrl.substring(
+                    fragmentIndex + 1,
+                    _inputUrl.length
+                );
+                _inputUrl = _inputUrl.substring(0, fragmentIndex);
+            }
+            if (queryIndex >= 0) {
+                queryStr = _inputUrl.substring(
+                    queryIndex + 1,
+                    _inputUrl.length
+                );
+                _inputUrl = _inputUrl.substring(0, queryIndex);
+                queryStr.split("&").map(i => {
+                    if (i.indexOf("=")) {
+                        const _queryItem = i.split("=");
+                        if (_queryItem.length == 2) {
+                            queryObj[_queryItem[0]] = _queryItem[1];
+                        }
+                    }
+                });
+            } else {
+                queryObj = undefined;
+            }
+            const regex = /(.+):\/\/(.+?)\/(.+)/,
+                matches = regex.exec(_inputUrl);
+            if (matches) {
+                const scheme = matches[1],
+                    host = matches[2],
+                    path = matches[3],
+                    urlResult = {
+                        scheme: scheme,
+                        host: host,
+                        path: path.startsWith("/") ? path : `/${path}`,
+                        fragment: fragment,
+                        query: queryObj,
+                        queryStr: queryStr
+                    };
+                return urlResult;
+            } else {
+                return matches || undefined;
+            }
+        },
+        isUrl: _inputUrl => {
+            const _urlResult = Url.getUrlByRegex(_inputUrl);
+            return _urlResult ? _urlResult.host && _urlResult.scheme : false;
+        }
     };
 module.exports = {
     __VERSION__,
@@ -311,5 +367,6 @@ module.exports = {
     Str,
     Image,
     File,
-    View
+    View,
+    Url
 };
