a511a2f55c893f914f18918f5a3e903c18718c5f
zhihaofans <zhihaofans@hotmail.com>
2021-03-06 20:12:24 +0800

    Update $$.js

--- scripts/libs/$$.js
+++ scripts/libs/$$.js
@@ -282,6 +282,26 @@ let __VERSION__ = 2,
             } else {
                 return false;
             }
+        },
+        getFileList: (dir, ext = undefined) => {
+            if ($file.exists(dir) && $file.isDirectory(dir)) {
+                var files = [];
+                const fileList = $file.list(dir);
+                fileList.map(f => {
+                    if (!$file.isDirectory(f)) {
+                        if (ext) {
+                            if (f.endsWith(`.${ext}`)) {
+                                files.push(f);
+                            }
+                        } else {
+                            files.push(f);
+                        }
+                    }
+                });
+                return files;
+            } else {
+                $console.error(`不存在该目录或不是目录:${dir}`);
+            }
         }
     },
     View = {
@@ -362,10 +382,6 @@ let __VERSION__ = 2,
             } else {
                 return matches || undefined;
             }
-        },
-        isUrl: _inputUrl => {
-            const _urlResult = Url.getUrlByRegex(_inputUrl);
-            return _urlResult ? _urlResult.host && _urlResult.scheme : false;
         }
     },
     Time = {
