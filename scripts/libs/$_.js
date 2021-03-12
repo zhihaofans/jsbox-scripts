09c25153292427039ffe6857412f0662cb941a48
zhihaofans <zhihaofans@hotmail.com>
2021-03-12 10:38:16 +0800

    $_.js

--- scripts/libs/$_.js
+++ scripts/libs/$_.js
@@ -1,41 +1,14 @@
-class ModV4 {
-    constructor(indexJsPath) {
-        this._indexJsPath = indexJsPath;
+class SuperString extends String {
+    constructor(_str) {
+        super(_str);
     }
-    get indexJs() {
-        const _f = require("$$").File;
-        return this._indexJsPath
-            ? _f.isFile(this.vindexJsPath)
-                ? require(this._indexJsPath) || undefined
-                : undefined
-            : undefined;
-    }
-    showModList(_title, modDir, listData) {
-        $console.error(modDir);
-        $console.error(listData);
-        $ui.push({
-            props: {
-                title: _title
-            },
-            views: [
-                {
-                    type: "list",
-                    props: {
-                        data: listData.map(_mod => _mod.modTitle)
-                    },
-                    layout: $layout.fill,
-                    events: {
-                        didSelect: function (_sender, indexPath, _data) {
-                            const row = indexPath.row,
-                                thisMod = listData[row];
-                            require(modDir + thisMod.modId)[
-                                thisMod.modAction
-                            ]();
-                        }
-                    }
-                }
-            ]
-        });
+    toJson() {
+        try {
+            return JSON.parse(this);
+        } catch (_error) {
+            $console.error(`SuperString:${_error.message}`);
+            return undefined;
+        }
     }
 }
 const Url = {
@@ -88,7 +61,8 @@ const Url = {
         return _urlResult ? _urlResult.host && _urlResult.scheme : false;
     }
 };
+
 module.exports = {
-    ModV4,
-    Url
+    Url,
+    SuperString
 };
