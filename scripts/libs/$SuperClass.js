72ef4f6097f61b2e621bba0259a259b2970e7cc6
zhihaofans <zhihaofans@hotmail.com>
2021-03-26 16:28:17 +0800

    $SuperClass: Add SuperString.format()

--- scripts/libs/$SuperClass.js
+++ scripts/libs/$SuperClass.js
@@ -2,6 +2,12 @@ class SuperString extends String {
   constructor(_inputString) {
     super(_inputString);
   }
+  format() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function (match, number) {
+      return typeof args[number] != "undefined" ? args[number] : match;
+    });
+  }
 }
 module.exports = {
   SuperString
