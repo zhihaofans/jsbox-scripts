dffbb7ec40ad690123d23c3c292ec2fb687afa9e
zhihaofans <zhihaofans@hotmail.com>
2021-05-04 16:54:13 +0800

    pref($_): update version to 4

--- scripts/libs/$_.js
+++ scripts/libs/$_.js
@@ -1,68 +1,261 @@
-class SuperString extends String {
-  constructor(_str) {
-    super(_str);
+const __VERSION__ = 4;
+class Console {
+  auto(success = true, message) {
+    success ? $console.info(message) : $console.error(message);
   }
-  toJson() {
-    try {
-      return JSON.parse(this);
-    } catch (_error) {
-      $console.error(`SuperString:${_error.message}`);
+}
+class File {
+  ifFile(path) {
+    if (path && $file.exists(path)) {
+      if ($file.isDirectory(path)) {
+        return false;
+      } else {
+        return true;
+      }
+    } else {
+      return false;
+    }
+  }
+  getFileList(dir, ext = undefined) {
+    if ($file.exists(dir) && $file.isDirectory(dir)) {
+      let files = [];
+      const fileList = $file.list(dir);
+      fileList.map(f => {
+        if (!$file.isDirectory(f)) {
+          if (ext) {
+            if (f.endsWith(`.${ext}`)) {
+              files.push(f);
+            }
+          } else {
+            files.push(f);
+          }
+        }
+      });
+      return files;
+    } else {
+      $console.error(`不存在该目录或不是目录:${dir}`);
       return undefined;
     }
   }
 }
-const Url = {
-  regex: inputUrl => {
-    let _inputUrl = inputUrl,
-      fragmentIndex = _inputUrl.indexOf("#"),
-      fragment = undefined,
-      queryIndex = _inputUrl.indexOf("?"),
-      queryStr = undefined,
-      queryObj = {};
-    if (fragmentIndex >= 0) {
-      fragment = _inputUrl.substring(fragmentIndex + 1, _inputUrl.length);
-      _inputUrl = _inputUrl.substring(0, fragmentIndex);
-    }
-    if (queryIndex >= 0) {
-      queryStr = _inputUrl.substring(queryIndex + 1, _inputUrl.length);
-      _inputUrl = _inputUrl.substring(0, queryIndex);
-      queryStr.split("&").map(i => {
-        if (i.indexOf("=")) {
-          const _queryItem = i.split("=");
-          if (_queryItem.length == 2) {
-            queryObj[_queryItem[0]] = _queryItem[1];
-          }
-        }
-      });
-    } else {
-      queryObj = undefined;
-    }
-    const regex = /(.+):\/\/(.+?)\/(.+)/,
-      matches = regex.exec(_inputUrl);
-    if (matches) {
-      const scheme = matches[1],
-        host = matches[2],
-        path = matches[3],
-        urlResult = {
-          scheme: scheme,
-          host: host,
-          path: path.startsWith("/") ? path : `/${path}`,
-          fragment: fragment,
-          query: queryObj,
-          queryStr: queryStr
-        };
-      return urlResult;
-    } else {
-      return matches || undefined;
-    }
-  },
-  isUrl: _inputUrl => {
-    const _urlResult = Url.regex(_inputUrl);
-    return _urlResult ? _urlResult.host && _urlResult.scheme : false;
+class Http {
+  constructor(timeout = 5) {
+    this.TIMEOUT = timeout;
   }
-};
-
+  async get(url, header) {
+    const result = await $http.get({
+      url: url,
+      timeout: this.TIMEOUT,
+      header: header
+    });
+    return url ? result : undefined;
+  }
+  async post(url, postBody, header = undefined) {
+    const result = await $http.post({
+      url: url,
+      header: header,
+      timeout: this.TIMEOUT,
+      body: postBody
+    });
+    return url ? result : undefined;
+  }
+}
+class Push {
+  default(title = "标题", body = "内容", mute = true) {
+    $push.schedule({
+      title: title,
+      body: body,
+      mute: mute
+    });
+  }
+}
+class Share {
+  isAction() {
+    return $app.env == $env.action;
+  }
+  isSafari() {
+    return $app.env == $env.safari;
+  }
+  isActionOrSafari() {
+    return Share.isAction() || Share.isSafari();
+  }
+  getImage() {
+    if (Share.isAction()) {
+      return $context.image || undefined;
+    }
+    return undefined;
+  }
+  getText() {
+    if (Share.isAction()) {
+      return $context.text || undefined;
+    }
+    if (Share.isSafari()) {
+      return $context.safari.title || undefined;
+    }
+    return undefined;
+  }
+  getLink() {
+    if (Share.isAction()) {
+      return $context.link || undefined;
+    }
+    if (Share.isSafari()) {
+      return $context.safari ? $context.safari.items.location.href : undefined;
+    }
+    return undefined;
+  }
+  getLinks() {
+    if (Share.isAction()) {
+      return $context.linkItems || undefined;
+    }
+    if (Share.isSafari()) {
+      return $context.safari
+        ? [$context.safari.items.location.href]
+        : undefined;
+    }
+    return undefined;
+  }
+}
+class String {
+  copy(str) {
+    $clipboard.copy({
+      text: str,
+      ttl: 30,
+      locally: true
+    });
+  }
+  paste() {
+    return $clipboard.text || "";
+  }
+  toQrcode(str) {
+    return $qrcode.encode(str);
+  }
+  fromQrcode(qrcode) {
+    return $qrcode.decode(qrcode);
+  }
+  toJson(str) {
+    return JSON.parse(str);
+  }
+  fromJson(json) {
+    return JSON.stringify(json);
+  }
+  getListFromL10n(sourceList) {
+    return sourceList ? sourceList.map(x => $l10n(x)) : [];
+  }
+}
+class Time {
+  getUnixTime() {
+    return new Date().getTime();
+  }
+  getSecondUnixTime() {
+    return Math.round(new Date().getTime() / 1000);
+  }
+  toLocaltime(ISO8601, timezone = "Asia/Shanghai") {
+    const moment = require("moment");
+    return moment(ISO8601).tz(timezone).format("YYYY-MM-DD hh:mm:ss");
+  }
+}
+class View {
+  constructor() {
+    this.List = {
+      push: (title, listData, didSelect) => {
+        $ui.push({
+          props: {
+            title: title
+          },
+          views: [
+            {
+              type: "list",
+              props: {
+                data: listData
+              },
+              layout: $layout.fill,
+              events: {
+                didSelect: didSelect
+              }
+            }
+          ]
+        });
+      }
+    };
+    this.Image = {
+      showSingleMenu: imageUrl => {
+        if (imageUrl) {
+          const links = $detector.link(imageUrl);
+          let imageLink = imageUrl;
+          if (links.length > 1) {
+            imageLink = imageUrl[0];
+          }
+          $ui.menu({
+            items: ["用Safari打开", "分享", "快速预览", "网页预览"],
+            handler: function (title, idx) {
+              switch (idx) {
+                case 0:
+                  $app.openURL(imageLink);
+                  break;
+                case 1:
+                  $share.sheet([imageLink]);
+                  break;
+                case 2:
+                  $quicklook.open({
+                    url: imageLink,
+                    handler: function () {
+                      $console.info(imageLink);
+                    }
+                  });
+                  break;
+                case 3:
+                  $ui.preview({
+                    title: title,
+                    url: imageLink
+                  });
+                  break;
+                default:
+                  return;
+              }
+            }
+          });
+        } else {
+          return;
+        }
+      },
+      ShowMultGallery: urlList => {
+        $ui.push({
+          props: {
+            title: ""
+          },
+          views: [
+            {
+              type: "gallery",
+              props: {
+                items: urlList.map(u => ({
+                  type: "image",
+                  props: {
+                    src: u
+                  }
+                })),
+                interval: 3,
+                radius: 5.0
+              },
+              layout: function (make, view) {
+                make.left.right.inset(10);
+                make.centerY.equalTo(view.super);
+                make.height.equalTo(320);
+              }
+            }
+          ]
+        });
+      }
+    };
+  }
+}
 module.exports = {
-  Url,
-  SuperString
+  __VERSION__,
+  Console,
+  File,
+  Http,
+  Push,
+  Share,
+  String,
+  Time,
+  View
 };
