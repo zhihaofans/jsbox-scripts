805b28dd6d05efedb290effeff8b862df0d747f7
zhihaofans <zhihaofans@hotmail.com>
2021-05-16 23:36:12 +0800

    test(Demo): remake with EasyJsBox

--- scripts/Demo/main.js
+++ scripts/Demo/main.js
@@ -1,1 +1,12 @@
-$ui.render("main");
+// Check Framework
+const initEasyJsBox = require("../EasyJsBox/src/kernel").init;
+if (typeof initEasyJsBox !== "function") {
+  // requireEasyJsBox()
+  $app.close();
+} else {
+  // init
+  initEasyJsBox();
+  // run app
+  const app = require("./scripts/app");
+  app.run();
+}
--- scripts/Demo/scripts/app.js
+++ scripts/Demo/scripts/app.js
@@ -1,7 +1,40 @@
-function sayHello() {
-  $ui.alert($l10n('HELLO_WORLD'));
+const { Kernel, VERSION } = require("../EasyJsBox/src/kernel"),
+  // Storage = require("./storage"),
+  run = () => {
+    if ($app.env === $env.app) {
+      const kernel = new AppKernel();
+      const Factory = require("./ui/factory");
+      new Factory(kernel).render();
+    } else {
+      $ui.render({
+        views: [
+          {
+            type: "label",
+            props: {
+              text: "不支持在此环境中运行",
+              align: $align.center
+            },
+            layout: (make, view) => {
+              make.center.equalTo(view.super);
+              make.size.equalTo(view.super);
+            }
+          }
+        ]
+      });
+    }
+  };
+class AppKernel extends Kernel {
+  constructor() {
+    super();
+    this.query = $context.query;
+    // 注册组件
+    this.settingComponent = this.registerComponent("Setting");
+    this.setting = this.settingComponent.controller;
+  }
+  print(message) {
+    $console.info(message);
+  }
 }
-
 module.exports = {
-  sayHello: sayHello
-}
+  run
+};
